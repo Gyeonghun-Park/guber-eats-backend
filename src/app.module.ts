52df1015807235a42f9d3534c7add04aa91e4da5
Gyeonghun Park <gp112795@gmail.com>
2021-01-03 19:43:57 -0500

    TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'gyeonghunpark',
+      password: '123',
+      database: 'guber-eats',
+      synchronize: true,
+      logging: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
