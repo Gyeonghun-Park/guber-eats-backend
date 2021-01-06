0c5315fcaaca870d86acaab03f97ebe317d330d0
Gyeonghun Park <gp112795@gmail.com>
2021-01-06 15:54:57 -0500

    Log In

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -5,6 +5,7 @@ import {
 } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './users.service';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
 
 @Resolver(of => User)
 export class UserResolver {
@@ -20,17 +21,23 @@ export class UserResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const { ok, error } = await this.usersService.createAccount(
-        createAccountInput,
-      );
-      return {
-        ok,
-        error,
-      };
+      return this.usersService.createAccount(createAccountInput);
     } catch (error) {
       return {
         error,
         ok: false,
+      };
+    }
+  }
+
+  @Mutation(returns => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
       };
     }
   }
